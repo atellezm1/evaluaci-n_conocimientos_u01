e469491dae0665415d05af4bbe5af885fa6d1382
atellezm1 <atellezm@unjbg.edu.pe>
2024-06-24 03:01:41 -0500

    Actualiza ejercicio2

--- Untitled 2.cpp
+++ Untitled 2.cpp
@@ -2,22 +2,31 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n1, n2, c,i;
+	int n1, n2,i,aux;
 	cout<<" ingrese el primer numero:\n";cin>>n1;
 	cout<<" ingrese el segundo numero:\n";cin>>n2;
-	c=0;
-	i=1;
-	while(i<=n1 && i<=n2){
-		if(n1%i==0 && n2%i==0){
-			c=c+1;
+	
+	if(n1<n2){
+		i=n1;
+		while((n1%i!=0)||(n2%i!=0)){
+			i=i-1;
 		}
-		i=i+1;
-	}
-	if(c>1){
-		cout<<"NO SON PSESI ";
 	}
 	else{
-		cout<<"SI SON PESI ";
+		aux=n1;
+		n1=n2;
+		n2=aux;
+		i=n1;
+		while((n1%i!=0)||(n2%i!=0)){
+			i=i-1;
+		}	
 	}
+	if(i==1){
+			cout<<"son pesi";
+	}
+	else{
+			cout<<"no son pesi";
+	}
+	
 	return 0;
 }
